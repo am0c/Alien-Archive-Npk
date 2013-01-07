5b80a4c4f867189ec212b55846a2454c05a5c044
Hojung Youn <amoc.yn@gmail.com>
2013-01-08 01:19:51 +0900

    [#12] config() returns wrong paths
    The library path is made from concatenating per-dist
    share directory, and sub-share directory, plus 'lib/',
    plus library file name. But the 'lib/' directery was
    omitted, so, base directory names and base file name was
    correct but its absolute path was incorrect by missing
    fragments of each paths.
    
    This commit makes some changes to its API, yet its docu-
    mentation is not authored.

--- lib/Alien/Archive/Npk.pm
+++ lib/Alien/Archive/Npk.pm
@@ -6,20 +6,39 @@ use File::Spec::Functions;
 
 our $VERSION = '0.000001_001';
 
-my $SUBDIR = Alien::Archive::Npk::ConfigData->config('share_subdir');
-my $DIST = dist_dir('Alien-Archive-Npk');
+our $SUBDIR = Alien::Archive::Npk::ConfigData->config('share_subdir');
+our $DIST = dist_dir('Alien-Archive-Npk');
+
+my $kv = do {
+    my @prefix = ($DIST, $SUBDIR);
+
+    my $lib_dir = Alien::Archive::Npk::ConfigData->config('lib_dir');
+    my $include_dir = Alien::Archive::Npk::ConfigData->config('include_dir');
+    my $libs = Alien::Archive::Npk::ConfigData->config('libs');
+    my $includes = Alien::Archive::Npk::ConfigData->config('includes');
+
+    return {
+        prefix      => catdir(@prefix),
+        lib_dir     => catdir(@prefix, $lib_dir),
+        include_dir => catdir(@prefix, $include_dir),
+        libs        => $libs,
+        includes    => $includes,
+        libs_path     => [ map { catfile(@prefix, $lib_dir, $_) } @$libs ],
+        includes_path => [ map { catfile(@prefix, $include_dir, $_) } @$includes ],
+    };
+};
 
 sub config {
     my ($self, $key) = @_;
 
-    my $value = Alien::Archive::Npk::ConfigData->config($key);
-    return unless defined $value;
-
-    if (ref $value eq 'ARRAY') {
-        return [ map { catfile($DIST, $SUBDIR, $_) } @$value ];
+    if (exists $kv->{$key}) {
+        return $kv->{$key};
+    }
+    elsif (my $v = Alien::Archive::Npk::ConfigData->config($key)) {
+        return $v;
     }
     else {
-        return catfile($DIST, $SUBDIR, $value);
+        return;
     }
 }
 
