a86663dcdb1088ac15708fac606a9c7d69ab985b
Hojung Youn <amoc.yn@gmail.com>
2013-01-09 14:40:54 +0900

    Add module documentation as POD
    Fixes #5

--- lib/Alien/Archive/Npk.pm
+++ lib/Alien/Archive/Npk.pm
@@ -47,15 +47,96 @@ __END__
 
 =head1 NAME
 
-Alien::Archive::Npk - Alien for neat package system - npk
+Alien::Archive::Npk - Alien for Neat Package System - npk
 
 =head1 SYNOPSIS
 
+  use Alien::Archive::Npk;
 
+  my @config_key = qw(
+    prefix
+    lib_dir include_dir
+    libs includes
+    libs_paths includes_paths
+  );
+
+  for (@config_key) {
+    print Alien::Archive::Npk->config(
+        ref($_) eq 'ARRAY' ? "@$_" : $_;
+      ), "\n";
+  }
 
 =head1 DESCRIPTION
 
+B<npk> is simple file packager and its file format, pronounced as I<en-pack>.
 
+If you're finding a platform independent, easy-to-use and also secure
+packageing system, npk is for you. It works on most of modern operating
+systems, and also supports powerful tools for manipulating packages.
+
+CPAN modules based on C<Alien::> namespace are to intall specified external
+libraries that are not pure perl and provide the caller with enough
+information to use it. See L<Alien>.
+
+When you install L<Alien::Archive::Npk>, it will ensure that your system is
+ready to use npk library, regardless of the platform it is installed on.
+It will fetch B<libnpk> from the archive by network to build, so
+network is required to install this module.
+
+If you're writing some module which requires npk library to operate,
+You can add Alien::Archive::Npk as a dependency to it rather
+than code configuration stuffs manually.
+
+The module has C<config()> method which provides configuration values or
+noticable paths of npk library installed by Alien::Archive::Npk.
+
+=head1 METHODS
+
+=over 4
+
+=item config("prefix")
+
+The root directory path configured to be of the installed npk.
+The module will build npk again even though there is already one on the system.
+So this install prefix might be sub-directory of the one of C<@INC>.
+
+=item config("lib_dir")
+
+The path where the loadable shared library of npk will be placed.
+
+=item config("include_dir")
+
+The path where the header files of npk will be placed.
+
+=item config("libs")
+
+The list of shared library file basenames returned as an array reference.
+
+=item config("libs_path")
+
+The list of shared library file paths returned as an array reference.
+
+=item config("includes")
+
+The list of header file basenames returned as an array reference.
+
+=item config("includes_path")
+
+The list of header files paths returned as an array reference.
+
+=back
+
+=head1 BUGS
+
+Please report bugs to L<github issues|http://github.com/am0c/Alien-Archive-Npk/issues>.
+
+=head1 AUTHOR
+
+Hojung Youn C<< <amorette@cpan.org> >>
+
+=head1 LICENSE
+
+Same as perl itself.
 
 =cut
 
