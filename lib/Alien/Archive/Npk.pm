47f3cbae67f732c4e92c735df4a9ea63612c62e4
Hojung Youn <amoc.yn@gmail.com>
2013-01-10 19:51:17 +0900

    Fix closure variable to be work as we want
    The `config()` method cannot access to the $kv
    variable that was removed from the memory, but remained
    to undef value as a closure.

--- lib/Alien/Archive/Npk.pm
+++ lib/Alien/Archive/Npk.pm
@@ -9,27 +9,29 @@ our $VERSION = '0.000001_002';
 our $SUBDIR = Alien::Archive::Npk::ConfigData->config('share_subdir');
 our $DIST = dist_dir('Alien-Archive-Npk');
 
-my $kv = do {
-    my @prefix = ($DIST, $SUBDIR);
-
-    my $lib_dir = Alien::Archive::Npk::ConfigData->config('lib_dir');
-    my $include_dir = Alien::Archive::Npk::ConfigData->config('include_dir');
-    my $libs = Alien::Archive::Npk::ConfigData->config('libs');
-    my $includes = Alien::Archive::Npk::ConfigData->config('includes');
-
-    return {
-        prefix      => catdir(@prefix),
-        lib_dir     => catdir(@prefix, $lib_dir),
-        include_dir => catdir(@prefix, $include_dir),
-        libs        => $libs,
-        includes    => $includes,
-        libs_path     => [ map { catfile(@prefix, $lib_dir, $_) } @$libs ],
-        includes_path => [ map { catfile(@prefix, $include_dir, $_) } @$includes ],
-    };
-};
+my $kv;
 
 sub config {
     my ($self, $key) = @_;
+
+    if (!defined $kv) {
+        my @prefix = ($DIST, $SUBDIR);
+
+        my $lib_dir = Alien::Archive::Npk::ConfigData->config('lib_dir');
+        my $include_dir = Alien::Archive::Npk::ConfigData->config('include_dir');
+        my $libs = Alien::Archive::Npk::ConfigData->config('libs');
+        my $includes = Alien::Archive::Npk::ConfigData->config('includes');
+
+        $kv = {
+            prefix      => catdir(@prefix),
+            lib_dir     => catdir(@prefix, $lib_dir),
+            include_dir => catdir(@prefix, $include_dir),
+            libs        => $libs,
+            includes    => $includes,
+            libs_path     => [ map { catfile(@prefix, $lib_dir, $_) } @$libs ],
+            includes_path => [ map { catfile(@prefix, $include_dir, $_) } @$includes ],
+        };
+    }
 
     if (exists $kv->{$key}) {
         return $kv->{$key};
